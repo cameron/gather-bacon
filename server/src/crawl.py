8286001a6f7fa7c9aed2cee2b5f324f6ba5ec408
Cameron Boehmer <cameron.boehmer@gmail.com>
2017-04-26 12:35:49 -0700

    remove redis

--- server/src/crawl.py
+++ server/src/crawl.py
@@ -7,7 +7,17 @@ from lib import resources
 resources.setup_dbpool()
 
 from models.domain import Domain
-Domain.by_url('http://news.ycombinator.com')
+d = Domain.by_url('http://news.ycombinator.com')
+# left off debugging why _meta is empty here
+print d.wwws.of_type._meta
+
+from models.www import WWW
+www = WWW.singleton()
+print www.domains.of_type._meta 
+
+
+#Page.by_url('http://news.ycombinator.com').remove()
+#page = www.add_url('http://news.ycombinator.com')
 
 from models.crawler import Crawler
 Crawler.wake_or_create().crawl()
